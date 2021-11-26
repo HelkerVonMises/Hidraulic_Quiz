078f6b559abdc684d3d4f2ba4ebb42f0a4638eed
HelkerVonMises <jmundialj@gmail.com>
2021-11-26 20:19:43 -0300

    First commit

---
+++ HidralicQuiz.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <conio.h>
+#include <unistd.h>     //comando sleep
+#include <stdlib.h>     //comando clear
+
+char ichi ();
+char ni();
+char san();
+char yon();
+char go();
+char roku();
+char nana();
+char hachi();
+char kyu();
+char juu();
+
+int main(){
+    
+    printf("\t\tBem vindo ao quiz lendário sobre hidráulica versão Beta 1.0\n");
+    sleep(2);
+    printf("\t\t\tO tema do quiz é sobre fluídos hidráulicos\n");
+    sleep(2);
+    printf("\t\t\t\t\tBoa sorte :)\n\n");
+    sleep(2);
+}
+
+char ichi (){
+    printf("Pergunta 1\n");
+}
+char ni (){
+    printf("Pergunta 2\n");
+}
+char san (){
+    printf("Pergunta 3\n");
+}
+char yon (){
+    printf("Pergunta 4\n");
+}
+char go (){
+    printf("Pergunta 5\n");
+}
+char roku (){
+    printf("Pergunta 6\n");
+}
+char nana (){
+    printf("Pergunta 7\n");
+}
+char hachi (){
+    printf("Pergunta 8\n");
+}
+char kyu (){
+    printf("Pergunta 9\n");
+}
+char juu (){
+    printf("Pergunta 10\n");
+}
