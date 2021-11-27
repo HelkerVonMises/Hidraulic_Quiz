828fe2267f18f7e233cc5b89b38f110f00a13a10
HelkerVonMises <94910396+HelkerVonMises@users.noreply.github.com>
2021-11-27 20:08:10 -0300

    Create quests.c

---
+++ quests.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<conio.h>
+
+int quest1();
+int quest2();
+int quest3();
+int quest4();
+int quest5();
+int quest6();
+
+int main(){
+quest1();
+quest2();
+quest3();
+quest4();
+quest5();
+quest6();
+}
+
+int quest1(){       //Viscosidade
+    printf("\t\tUma das funções do óleo com menor viscosidade no sistema é:\n\n");
+    printf("A) Desgastar as peças do sistema\n");
+    printf("B) Dificultar o escoamento\n");
+    printf("C) Lubrificar todas as peças do sistema de forma rápida e uniforme\n");
+    printf("D) Demandar maior manutenção\n");
+    printf("E) Gerar maior esforço do motor\n\n");
+    printf("A resposta é a alternativa C)\n\n");
+}
+
+int quest2(){       //Propriedades do óleo
+    printf("\t\tA textura do óleo é importante para o sistema?\n\n");
+    printf("A) Sim, pois o óleo deve gerar uma película protetora no sistema;\n");
+    printf("B) Não, pois o óleo atrapalha o sistema;\n");
+    printf("C) Sim, pois o óleo deve gerar uma resistência no motor;\n");
+    printf("D) Não é importante para o sistema;\n");
+    printf("E) Nenhuma das alternativas acima;\n\n");
+    printf("A resposta é a alternativa A)\n\n");
+}
+
+int quest3(){       //Composição do óleo
+    printf("\t\tQuais são as três bases do Óleo Mineral?\n\n");
+    printf("A) Parafínico, Graxante e Misto;\n");
+    printf("B) Sintético, Vegetal e Misto;\n");
+    printf("C) Sintético, Naftênico e Vegetal; \n");
+    printf("D) Sintético, Naftênico e Misto;\n");
+    printf("E) Parafínico, Naftênico e Misto;\n\n");
+    printf("A resposta é a alternativa E)\n\n");
+}
+
+int quest4(){       //Ponto de fulgor
+    printf("\t\tPonto de fulgor é a menor temperatura na qual o combustível libera vapor.\n");
+    printf("\t\t\tSabendo disso, qual o ponto de fulgor do Óleo Mineral?\n\n");
+    printf("A) Entre 160ºC e 180ºC.\n");
+    printf("B) Entre 220ºF e 240ºF.\n");
+    printf("C) Entre 30ºC e 50ºC.\n");
+    printf("D) Entre 220ºC e 240ºC.\n");
+    printf("E) Nenhuma das respostas acima.\n\n");
+    printf("A resposta é a alternativa D)\n\n");
+}
+
+int quest5(){       //Demulsibilidade
+    printf("\t\tQual a característica da demulsibilidade?\n\n");
+    printf("A) A capacidade de misturar óleo e água;\n");
+    printf("B) A capacidade de dissolver água;\n");
+    printf("C) A capacidade de separar água e óleo;\n");
+    printf("D) A capacidade de absorver água;\n");
+    printf("E) Nenhuma das anteriores.\n\n");
+    printf("A resposta é a alternativa C)\n\n");
+}
+
+int quest6(){       //Demulsibilidade
+    printf("\t\tO que ocorrerá com a diminuição da demulsibilidade?\n\n");
+    printf("a) O aumento da resistência dos componentes;\n");
+    printf("b) A higienização instantânea dos componentes;\n");
+    printf("c) A diminuição da vida útil do equipamento;\n");
+    printf("d) A expansão dos equipamentos;\n");
+    printf("E) Nenhuma das alternativas.\n\n");
+    printf("A resposta é a alternativa C)\n\n");
+}
