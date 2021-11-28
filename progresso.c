3edaab1c08e43fd14331f15999fafc55b4b5589e
HelkerVonMises <94910396+HelkerVonMises@users.noreply.github.com>
2021-11-28 00:41:39 -0300

    Create progresso.c

--- progresso.c
+++ progresso.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <conio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <time.h>
 
 char ichi ();
@@ -12,7 +13,7 @@ char roku();
 
     int main(){
         
-    char c[6];
+    char var1, var2, var3, var4, var5, var6;
     int a[10];
     
     printf("\t\tBem vindo ao quiz lendário sobre hidráulica versão Beta 1.0\n");
@@ -172,48 +173,48 @@ char roku();
         break;
     }
 char ichi ();
-    scanf ("%c", c);
-        if(c[0]=='c'){
+    scanf ("%c", &var1);
+    if(var1=='c'){
         printf("Isso ai!");
         }
         else{
             goto fim;
         }
-    char ni ();
-    scanf("%s", c);
-    if(c[1]=='a'){
+char ni ();
+    scanf("%c", &var2);
+    if(var2=='a'){
         printf("Cê ta doido");
     }
         else{
             goto fim;
         }
-    char san ();
-    scanf("%s", c);
-    if(c[2]=='e'){
+char san ();
+    scanf("%c", &var3);
+    if(var3=='e'){
         printf("Brabo");
     }
         else{
             goto fim;
         }
-    char yon ();
-    scanf("%s", c);
-    if(c[3]=='d'){
+char yon ();
+    scanf("%c", &var4);
+    if(var4=='d'){
         printf("Vai que vai");
     }
         else{
             goto fim;
         }
-    char go ();
-    scanf("%s", c);
-    if(c[4]=='c'){
+char go ();
+    scanf("%c", &var5);
+    if(var5=='c'){
         printf("Ta com tudo hein");
     }
         else{
             goto fim;
         }
-    char roku ();
-    scanf("%s", c);
-    if(c[5]=='c'){
+char roku ();
+    scanf("%c", &var6);
+    if(var6=='c'){
         printf("壮大");
     }
         else{
@@ -224,20 +225,20 @@ char ichi ();
 }
 
 char ichi (){
-    printf("\t\t\t\t\tViscosidade\n\t\tUma das funções do óleo com menor viscosidade no sistema é:\n\nA) Desgastar as peças do sistema\nB) Dificultar o escoamento\nC) Lubrificar todas as peças do sistema de forma rápida e uniforme\nD) Demandar maior manutenção\nE) Gerar maior esforço do motor\n\nResposta: \n\n");
-    }
+    printf("\t\t\t\tViscosidade\n\t\tUma das funções do óleo com menor viscosidade no sistema é:\n\nA) Desgastar as peças do sistema\nB) Dificultar o escoamento\nC) Lubrificar todas as peças do sistema de forma rápida e uniforme\nD) Demandar maior manutenção\nE) Gerar maior esforço do motor\n\nResposta: \n\n");
+}
 char ni (){
-    printf("\t\t\t\tPropriedades do óleo\n\n\t\tA textura do óleo é importante para o sistema?\n\nA) Sim, pois o óleo deve gerar uma película protetora no sistema;\nB) Não, pois o óleo atrapalha o sistema;\nC) Sim, pois o óleo deve gerar uma resistência no motor;\nD) Não é importante para o sistema;\nE) Nenhuma das alternativas acima;\n\nResposta: \n\n");
+    printf("\t\t\t\tPropriedades do óleo\n\n\t\tA textura do óleo é importante para o sistema?\n\nA) Sim, pois o óleo deve gerar uma película protetora no sistema\nB) Não, pois o óleo atrapalha o sistema\nC) Sim, pois o óleo deve gerar uma resistência no motor\nD) Não é importante para o sistema\nE) Nenhuma das alternativas acima\n\nResposta: \n\n");
 }
 char san (){
-    printf("\t\t\t\tComposição do óleo\n\n\t\tQuais são as três bases do Óleo Mineral?\n\nA) Parafínico, Graxante e Misto;\nB) Sintético, Vegetal e Misto;\nC) Sintético, Naftênico e Vegetal; \nD) Sintético, Naftênico e Misto;\nE) Parafínico, Naftênico e Misto;\n\nResposta: \n\n");
+    printf("\t\t\t\tComposição do óleo\n\n\t\tQuais são as três bases do Óleo Mineral?\n\nA) Parafínico, Graxante e Misto\nB) Sintético, Vegetal e Misto\nC) Sintético, Naftênico e Vegetal\nD) Sintético, Naftênico e Misto\nE) Parafínico, Naftênico e Misto\n\nResposta: \n\n");
 }
 char yon (){
-    printf("\t\t\t\t\tPonto de fulgor\n\n\t\tPonto de fulgor é a menor temperatura na qual o combustível libera vapor.\n\t\t\tSabendo disso, qual o ponto de fulgor do Óleo Mineral?\n\nA) Entre 160ºC e 180ºC.\nB) Entre 220ºF e 240ºF.\nC) Entre 30ºC e 50ºC.\nD) Entre 220ºC e 240ºC.\nE) Nenhuma das respostas acima.\n\nResposta: \n\n");
+    printf("\t\t\t\tPonto de fulgor\n\n\t\tPonto de fulgor é a menor temperatura na qual o combustível libera vapor.\n\t\t\tSabendo disso, qual o ponto de fulgor do Óleo Mineral?\n\nA) Entre 160ºC e 180ºC\nB) Entre 220ºF e 240ºF\nC) Entre 30ºC e 50ºC\nD) Entre 220ºC e 240ºC\nE) Nenhuma das respostas acima\n\nResposta: \n\n");
 }
 char go (){
-    printf("\t\t\t\tDemulsibilidade\n\n\t\tQual a característica da demulsibilidade?\n\n) A capacidade de misturar óleo e água;\nB) A capacidade de dissolver água;\nC) A capacidade de separar água e óleo;\nD) A capacidade de absorver água;\nE) Nenhuma das anteriores.\n\nResposta: \n\n");
+    printf("\t\t\t\tDemulsibilidade\n\n\t\tQual a característica da demulsibilidade?\n\n) A capacidade de misturar óleo e água\nB) A capacidade de dissolver água\nC) A capacidade de separar água e óleo\nD) A capacidade de absorver água\nE) Nenhuma das anteriores\n\nResposta: \n\n");
 }
 char roku (){
-    printf("\t\t\t\tDemulsibilidade\n\n\t\tO que ocorrerá com a diminuição da demulsibilidade?\n\na) O aumento da resistência dos componentes;\nb) A higienização instantânea dos componentes;\nc) A diminuição da vida útil do equipamento;\nd) A expansão dos equipamentos;\nE) Nenhuma das alternativas.\n\nResposta: \n\n");
+    printf("\t\t\t\tDemulsibilidade\n\n\t\tO que ocorrerá com a diminuição da demulsibilidade?\n\na) O aumento da resistência dos componentes\nb) A higienização instantânea dos componentes\nc) A diminuição da vida útil do equipamento\nd) A expansão dos equipamentos\nE) Nenhuma das alternativas\n\nResposta: \n\n");
 }
